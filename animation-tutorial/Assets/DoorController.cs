681723b55d3e09d21061021ae9b542acb76f472f
Karl Henkel <karlh07@gmail.com>
2017-02-28 13:23:15 -0500

    Add door controller script.

---
+++ animation-tutorial/Assets/DoorController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void OnGUI () {
+
+		if(GUILayout.Button("Open Door"))
+		{
+			animator.SetInteger("Door Close", 0);
+			animator.SetInteger("Door Open", 1);
+		}
+
+		if(GUILayout.Button("Close Door"))
+		{
+			animator.SetInteger("Door Close", 1);
+			animator.SetInteger("Door Open", 0);
+		}
+	}
+}
